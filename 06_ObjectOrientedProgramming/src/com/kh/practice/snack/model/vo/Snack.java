b1dd6e2b4487f318eddb48b24b16bfc275bcef94
alstjd0930 <alstjd0930@naver.com>
2023-06-02 11:35:02 +0900

    객체 실습 문제
    Run 클래스에서 동작함

---
+++ 06_ObjectOrientedProgramming/src/com/kh/practice/snack/model/vo/Snack.java
@@ -0,0 +1,68 @@
+package com.kh.practice.snack.model.vo;
+
+public class Snack {
+
+	private String kind;
+	private String name;
+	private String flavor;
+	private int numof;
+	private int price;
+	private String hh;
+	
+	
+	public Snack() {}
+	public Snack(String kind, String name, String flavor,int numof ,int price,String hh) {
+		
+		this.kind=kind;
+		this.name=name;
+		this.hh=hh;
+		
+		}
+	
+	
+	public String information() {
+		return "Snack [kind=" + kind + ", name=" + name + ", flavor=" + flavor + ", numof=" + numof + ", price=" + price
+				+ "]";
+		//TODO
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFlavor() {
+		return flavor;
+	}
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
+	}
+	public int getNumof() {
+		return numof;
+	}
+	
+	public String getHh() {
+		return hh;
+	}
+	public void setHh(String hh) {
+		this.hh = hh;
+	}
+	public void setNumof(int numof) {
+		this.numof = numof;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+}
